f92ac2ace952bc9ef5b8750cf25cf370d5734477
marijadragicevic <majadragicevic.111@gmail.com>
2022-01-31 16:46:28 +0100

    4.commit/Create async method for adding new message in database

--- 28_PROJEKAT/chat.js
+++ 28_PROJEKAT/chat.js
@@ -4,16 +4,60 @@ export class Classroom {
         this.username = usr;
         this.chats = db.collection("chats");
     }
+    // seteri polja
     set room(rm) {
         this._room = rm;
-    }
-    get room() {
-        return this._room;
     }
     set username(usr) {
         this._username = usr;
     }
+    // geteri polja
+    get room() {
+        return this._room;
+    }
     get username() {
         return this._username;
     }
+
+    // Dodavanje nove poruke
+    async addChat(mess) {
+        //kreiranje dokum/obj  prosledjujem bazi podataka
+        let docChat = {
+            message: mess,
+            username: this.username,
+            room: this.room,
+            // dohvatanjne trenutnog vremena 
+            created_at: firebase.firestore.Timestamp.fromDate(new Date())
+        }
+        // Da sacuvam dokument u db
+        let response = await this.chats.add(docChat);
+        return response; // vracam promis i mogu za njega da kazem .then i .catch
+    }
+
+    // kreirati callback metod getChats()
+    getChats(callback) {
+
+    }
+
+
+
+
+    // ne stavljmao  then i cacth u metodu u klasi vec kada pozivamo metodu pisemo then i catch
+    // async addChat(poruka) {
+    //     let newDoc = await db.collection("chats")
+    //         .add({
+    //             message: poruka,
+    //             username: this.username,
+    //             room: this.room,
+    //             created_at: firebase.firestore.Timestamp.fromDate(new Date())
+    //         })
+    //         .then(() => {
+    //             console.log(`Uspesno dodat chat`);
+    //         }).catch((err) => {
+    //             console.log(`Greska ${err}`);
+    //         });
+
+    //     return newDoc;
+
+    // }
 }
--- 28_PROJEKAT/script.js
+++ 28_PROJEKAT/script.js
@@ -1,7 +1,17 @@
 import { Classroom } from "./chat.js";
 
-let Chatroom1 = new Classroom("js", "mikamikic");
-console.log(Chatroom1.username, Chatroom1.room); // testiramo getere
-Chatroom1.username = "Dusan"; // testiramo setere
-Chatroom1.room = "general";
-console.log(Chatroom1.username, Chatroom1.room);
+let chatroom1 = new Classroom("js", "mikamikic");
+console.log(chatroom1.username, chatroom1.room); // testiramo getere
+chatroom1.username = "Dusan"; // testiramo setere
+chatroom1.room = "general";
+console.log(chatroom1.username, chatroom1.room);
+
+let chatroom2 = new Classroom("general", "Milena");
+// chatroom2.addChat("HR trening");
+// chatroom2.addChat(`Posaljite CV`)
+chatroom2.addChat(`Trening pisanja CV-a`)
+    .then(() => console.log(`Uspesno dodat chat`))
+    .catch(err => console.log(`Greska ${err}`))
+chatroom2.getChats(() => {
+
+});
