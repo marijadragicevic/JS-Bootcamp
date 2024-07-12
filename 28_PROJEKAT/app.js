a0aa45f630e26fa433d522f8b05791040e02c495
Marija Dragicevic <marija.dragicevic@mvsoft.rs>
2024-07-12 14:51:33 +0200

    Added introductions section

--- 28_PROJEKAT/app.js
+++ 28_PROJEKAT/app.js
@@ -32,6 +32,9 @@ const aside = document.querySelector(".aside");
 const searchDialog = document.querySelector(".search-dialog");
 const searchClose = document.querySelector(".search-dialog__close");
 
+const skipBtns = document.querySelectorAll(".btn-skip");
+const introductionContainer = document.querySelector(".section-guide");
+
 expandBtnList?.length > 0 &&
     expandBtnList?.forEach((expandBtn) => {
         expandBtn?.addEventListener("click", (event) => {
@@ -110,6 +113,13 @@ function scrollToLastMessage() {
         lastMessage.scrollIntoView({ behavior: "smooth" });
     }
 }
+
+const handleCloseAside = () => {
+    if (window.innerWidth <= 992) {
+        aside.classList.remove("active");
+        main.classList.add("active");
+    }
+};
 
 // prikazuju se najskorije poruke
 
@@ -214,6 +224,8 @@ formUsername.addEventListener("submit", (e) => {
         }, 3 * 1000);
     }
     inputUsername.value = "";
+
+    handleCloseAside();
 });
 
 //KLIK NA DUGME U NAVBARU = OTVORI SE TA SOBA
@@ -234,6 +246,7 @@ channelList.addEventListener("click", (e) => {
             chatUI.reorderMessages(check(localStorage.username, "anonymous"));
             scrollToLastMessage();
         });
+        handleCloseAside();
     }
 });
 
@@ -243,6 +256,7 @@ formColor.addEventListener("submit", function (e) {
     let inputColorValue = inputColor.value;
     messagesListContainer.style.backgroundColor = inputColorValue;
     localStorage.setItem("backgroundColor", inputColorValue);
+    handleCloseAside();
 });
 
 // KLIK NA DUGME SET DATES = PRIKAZ PORUKA U IZABRANOM VREMSKOM INTERVALU IZ TE SOBE
@@ -287,3 +301,130 @@ messagesList.addEventListener("click", (event) => {
         }
     }
 });
+
+// Swiper
+let swiper = new Swiper(".mySwiper", {
+    effect: "cards",
+    grabCursor: true,
+});
+
+// Close and show Introduction
+skipBtns?.forEach((skipBtn) => {
+    skipBtn.addEventListener("click", () => {
+        introductionContainer.classList.remove("active");
+        localStorage.setItem("introduction", "seen");
+    });
+});
+
+window.addEventListener("load", () => {
+    const sawIntroduction = localStorage.getItem("introduction");
+    if (!sawIntroduction) {
+        introductionContainer.classList.add("active");
+    } else {
+        introductionContainer.classList.remove("active");
+    }
+});
+
+// Confetti
+let W = window.innerWidth;
+let H = window.innerHeight;
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+const maxConfettis = 150;
+const particles = [];
+
+const possibleColors = [
+    "DodgerBlue",
+    "OliveDrab",
+    "Gold",
+    "Pink",
+    "SlateBlue",
+    "LightBlue",
+    "Gold",
+    "Violet",
+    "PaleGreen",
+    "SteelBlue",
+    "SandyBrown",
+    "Chocolate",
+    "Crimson",
+];
+
+function randomFromTo(from, to) {
+    return Math.floor(Math.random() * (to - from + 1) + from);
+}
+
+function confettiParticle() {
+    this.x = Math.random() * W; // x
+    this.y = Math.random() * H - H; // y
+    this.r = randomFromTo(11, 33); // radius
+    this.d = Math.random() * maxConfettis + 11;
+    this.color = possibleColors[Math.floor(Math.random() * possibleColors.length)];
+    this.tilt = Math.floor(Math.random() * 33) - 11;
+    this.tiltAngleIncremental = Math.random() * 0.07 + 0.05;
+    this.tiltAngle = 0;
+
+    this.draw = function () {
+        context.beginPath();
+        context.lineWidth = this.r / 2;
+        context.strokeStyle = this.color;
+        context.moveTo(this.x + this.tilt + this.r / 3, this.y);
+        context.lineTo(this.x + this.tilt, this.y + this.tilt + this.r / 5);
+        return context.stroke();
+    };
+}
+
+function Draw() {
+    const results = [];
+
+    // Magical recursive functional love
+    requestAnimationFrame(Draw);
+
+    context.clearRect(0, 0, W, window.innerHeight);
+
+    for (var i = 0; i < maxConfettis; i++) {
+        results.push(particles[i].draw());
+    }
+
+    let particle = {};
+    let remainingFlakes = 0;
+    for (var i = 0; i < maxConfettis; i++) {
+        particle = particles[i];
+
+        particle.tiltAngle += particle.tiltAngleIncremental;
+        particle.y += (Math.cos(particle.d) + 3 + particle.r / 2) / 2;
+        particle.tilt = Math.sin(particle.tiltAngle - i / 3) * 15;
+
+        if (particle.y <= H) remainingFlakes++;
+
+        // If a confetti has fluttered out of view,
+        // bring it back to above the viewport and let if re-fall.
+        if (particle.x > W + 30 || particle.x < -30 || particle.y > H) {
+            particle.x = Math.random() * W;
+            particle.y = -30;
+            particle.tilt = Math.floor(Math.random() * 10) - 20;
+        }
+    }
+
+    return results;
+}
+
+window.addEventListener(
+    "resize",
+    function () {
+        W = window.innerWidth;
+        H = window.innerHeight;
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+    },
+    false
+);
+
+// Push new confetti objects to `particles[]`
+for (var i = 0; i < maxConfettis; i++) {
+    particles.push(new confettiParticle());
+}
+
+// Initialize
+canvas.width = W;
+canvas.height = H;
+Draw();
